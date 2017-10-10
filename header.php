bd680cecfba6b67efc9e7d9b8b2bccc9682751aa
Amit Sinha <amits.sers@gmail.com>
2017-10-10 10:45:26 +0530

    created base files

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title><?php echo $page_title ?></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <style type="text/css">
+  	.header.container-fluid {
+  		padding: 0;
+  	}
+  	.header .header_menu_block {
+  		padding: 0 0 0 0;
+  	}
+
+  </style>
+</head>
+<body style="background: #000">
+	<div class="header container-fluid">
+		<div class="header_menu_block">
+			<div class="row">
+				<div class="col-sm-3">
+					Logo Here
+				</div>
+				<div class="col-sm-9">
+					<ul class="list-inline">
+						<li class="list-inline-item menu_item">Lorem ipsum</li>
+						<li class="list-inline-item menu_item">Phasellus iaculis</li>
+						<li class="list-inline-item menu_item">Nulla volutpat</li>
+					</ul>
+				</div>
+			</div>
+		</div>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+	$page_title = "This is title";
+	require_once "header.php";
